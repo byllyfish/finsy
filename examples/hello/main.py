9bfe31d8cc872f08cebb32f88976822afee74fe2
Bill Fisher <william.w.fisher@gmail.com>
2022-09-14 14:37:41 -0700

    Simplify demo app.

--- examples/hello/main.py
+++ examples/hello/main.py
@@ -1,20 +1,20 @@
 import asyncio
 import logging
+from ipaddress import IPv4Address
 from pathlib import Path
 
 import finsy as fy
 
+_ETH_ARP = 0x0806
 
-class HelloWorldApp:
+
+class DemoApp:
     "Hello World, demo app."
 
     CONTROLLER_PORT = 255
     P4SRC = Path(__file__).parent / "p4src"
     P4INFO = P4SRC / "hello.p4info.txt"
     P4BLOB = P4SRC / "hello.json"
-    PORTS = [1, 2, 3, 4, CONTROLLER_PORT]
-
-    options: fy.SwitchOptions
 
     def __init__(self):
         self.options = fy.SwitchOptions(
@@ -25,37 +25,24 @@ class HelloWorldApp:
 
     async def on_ready(self, switch: fy.Switch):
         "Switch's ready handler."
+        ports = [port.id for port in switch.ports] + [self.CONTROLLER_PORT]
+        learned = set[IPv4Address]()
+
         await switch.delete_all()
-        await switch.insert([fy.P4MulticastGroupEntry(1, replicas=self.PORTS)])
+        await switch.insert([fy.P4MulticastGroupEntry(1, replicas=ports)])
 
-        switch.create_task(self._send_packet_out(switch))
-
-        seen = set[bytes]()
-        async for packet in switch.read_packets():
-            addr = packet.payload[28:32]  # source IPv4 address from ARP
-            if addr and addr not in seen:
-                seen.add(addr)
+        async for packet in switch.read_packets(eth_types={_ETH_ARP}):
+            # Extract source IPv4 address from ARP.
+            addr = IPv4Address(packet.payload[28:32])
+            if addr and addr not in learned:
+                learned.add(addr)
                 port = packet["ingress_port"]
                 await switch.insert(self._learn(switch, addr, port))
 
-    async def _send_packet_out(self, switch: fy.Switch):
-        "Once a second, send a packet which will come right back to us."
-        while True:
-            await asyncio.sleep(1.0)
-            await switch.write(
-                [
-                    fy.P4PacketOut(
-                        bytes.fromhex("deadbeefdeadbeef"),
-                        egress_port=self.CONTROLLER_PORT,
-                        _pad=0,
-                    )
-                ]
-            )
-
     @staticmethod
-    def _learn(switch: fy.Switch, addr: bytes, port: int):
+    def _learn(switch: fy.Switch, addr: IPv4Address, port: int):
         "Return a P4TableEntry which tells where to forward this IPv4 address."
-        print(f"{switch.name} learned {addr.hex()} on port {port}")
+        print(f"{switch.name} learned {addr} on port {port}")
         return [
             fy.P4TableEntry(
                 "ipv4",
@@ -66,7 +53,7 @@ class HelloWorldApp:
 
 
 async def main():
-    app = HelloWorldApp()
+    app = DemoApp()
 
     switches = [
         fy.Switch("s1", "127.0.0.1:50001", app.options),
@@ -79,7 +66,7 @@ async def main():
 
 
 if __name__ == "__main__":
-    logging.basicConfig(level=logging.DEBUG)
+    logging.basicConfig(level=logging.INFO)
     try:
         asyncio.run(main())
     except KeyboardInterrupt:
