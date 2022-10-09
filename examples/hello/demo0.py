c8620175e54c906009fa614f03a369640f930cff
Bill Fisher <william.w.fisher@gmail.com>
2022-10-09 12:34:03 -0700

    Add error message.

--- examples/hello/demo0.py
+++ examples/hello/demo0.py
@@ -3,5 +3,10 @@ from pathlib import Path
 
 import finsy as fy
 
-p4info = fy.P4Schema(Path(sys.argv[1]))
+if len(sys.argv) != 2:
+    print("Usage: demo0.py <path>", file=sys.stderr)
+    sys.exit(1)
+
+p4info_path = Path(sys.argv[1])
+p4info = fy.P4Schema(p4info_path)
 print(p4info)
