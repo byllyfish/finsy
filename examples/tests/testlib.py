a73c6cb0fb93457846f35649ba697fd03f2fff05
Bill Fisher <william.w.fisher@gmail.com>
2022-12-21 20:19:03 -0700

    Include priority field in read_tables check.

--- examples/tests/testlib.py
+++ examples/tests/testlib.py
@@ -7,9 +7,11 @@ async def read_p4_tables(target: str) -> set[str]:
 
     async with fy.Switch("sw", target) as sw:
         with sw.p4info:
+            # Wildcard-read does not read default entries.
             async for entry in sw.read(fy.P4TableEntry()):
+                assert not entry.is_default_action
                 result.add(
-                    f"{entry.table_id} {entry.match_str(wildcard='*')} {entry.action_str()}"
+                    f"{entry.table_id} {entry.priority:#x} {entry.match_str(wildcard='*')} {entry.action_str()}"
                 )
 
     return result
