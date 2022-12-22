458cc22a1b3117717bca33120cded9767bb897af
Bill Fisher <william.w.fisher@gmail.com>
2022-12-21 20:55:37 -0700

    Check default entries also.

--- examples/tests/testlib.py
+++ examples/tests/testlib.py
@@ -13,5 +13,17 @@ async def read_p4_tables(target: str) -> set[str]:
                 result.add(
                     f"{entry.table_id} {entry.priority:#x} {entry.match_str(wildcard='*')} {entry.action_str()}"
                 )
+            # Read default entries for each table.
+            for table in sw.p4info.tables:
+                async for entry in sw.read(
+                    fy.P4TableEntry(
+                        table_id=table.alias,
+                        is_default_action=True,
+                    )
+                ):
+                    assert entry.is_default_action
+                    assert entry.priority == 0
+                    assert entry.match_str() == ""
+                    result.add(f"{entry.table_id} {entry.action_str()}")
 
     return result
