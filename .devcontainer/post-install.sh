499f5504b068f4516be907e3c75f658f34d94c8b
Bill Fisher <william.w.fisher@gmail.com>
2023-04-15 15:33:47 -0700

    install poetry using script.

--- .devcontainer/post-install.sh
+++ .devcontainer/post-install.sh
@@ -2,14 +2,8 @@
 
 set -ex
 
-WORKSPACE_DIR="$(pwd)"
-
 # Install poetry.
-pip3 install --user poetry
-
-# Prepare poetry to work inside a container.
-poetry config cache-dir "${WORKSPACE_DIR}/.cache"
-poetry config virtualenvs.in-project true
+python3 .devcontainer/install-poetry.py
 
 # Install all dependencies.
 poetry install
