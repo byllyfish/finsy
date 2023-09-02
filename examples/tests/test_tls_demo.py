26e9aa8688d3ffc875f81ab84c8edbce14e3e638
Bill Fisher <william.w.fisher@gmail.com>
2023-09-02 09:21:49 -0700

    Enable tls test with new demonet image.

--- examples/tests/test_tls_demo.py
+++ examples/tests/test_tls_demo.py
@@ -8,7 +8,6 @@ TLSDEMO_DIR = Path(__file__).parent.parent / "tls_demo"
 DEMONET = TLSDEMO_DIR / "net/run.py"
 
 
-@pytest.mark.xfail(reason="requires updated image")
 async def test_demo1(demonet, python):
     "Test the tls_demo/demo1 example program."
     async with python(TLSDEMO_DIR / "demo1.py") as demo1:
