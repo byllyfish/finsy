219d0d49c5594487fecb348b26773006b74ebf5b
Bill Fisher <william.w.fisher@gmail.com>
2024-07-06 11:54:27 -0700

    Upgrade grpcio-tools to 1.64.1 and re-compile protobuf files.

--- finsy/proto/gnmi1/gnmi_pb2_grpc.py
+++ finsy/proto/gnmi1/gnmi_pb2_grpc.py
@@ -5,7 +5,7 @@ import warnings
 
 from . import gnmi_pb2 as gnmi1_dot_gnmi__pb2
 
-GRPC_GENERATED_VERSION = '1.64.0'
+GRPC_GENERATED_VERSION = '1.64.1'
 GRPC_VERSION = grpc.__version__
 EXPECTED_ERROR_RELEASE = '1.65.0'
 SCHEDULED_RELEASE_DATE = 'June 25, 2024'
