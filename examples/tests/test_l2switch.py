56ae04dae38364fafefcd8e5e375abf9e0c06066
Bill Fisher <william.w.fisher@gmail.com>
2024-10-28 17:28:02 -0700

    Raise CancelledError to cancel server.

--- examples/tests/test_l2switch.py
+++ examples/tests/test_l2switch.py
@@ -17,7 +17,7 @@ async def test_demo(demonet, python):
     async with python(L2SWITCH_DIR / "demo.py") as demo:
         await asyncio.sleep(0.5)
         await demonet.send("pingall", expect="(6/6 received)")
-        demo.cancel()
+        raise asyncio.CancelledError()
 
 
 async def test_read_tables(demonet):
