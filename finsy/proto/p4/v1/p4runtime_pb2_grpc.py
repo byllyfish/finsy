4b338ee49aac23e45b364d0d1ae4cd1a89f751e1
Bill Fisher <william.w.fisher@gmail.com>
2025-06-03 11:23:37 -0700

    Re-compile protobufs with protobuf 6.30.0

--- finsy/proto/p4/v1/p4runtime_pb2_grpc.py
+++ finsy/proto/p4/v1/p4runtime_pb2_grpc.py
@@ -5,7 +5,7 @@ import warnings
 
 from . import p4runtime_pb2 as p4_dot_v1_dot_p4runtime__pb2
 
-GRPC_GENERATED_VERSION = '1.71.0'
+GRPC_GENERATED_VERSION = '1.72.1'
 GRPC_VERSION = grpc.__version__
 _version_not_supported = False
 
